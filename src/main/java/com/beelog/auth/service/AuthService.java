11f0a36cae717444b8766d680f4a69c68180dc46
hayrullahalper <alperhayrullah@gmail.com>
2023-12-15 22:37:59 +0300

    refactor: AuthService login

--- src/main/java/com/beelog/auth/service/AuthService.java
+++ src/main/java/com/beelog/auth/service/AuthService.java
@@ -1,36 +1,34 @@
 package com.beelog.auth.service;
 
-import at.favre.lib.crypto.bcrypt.BCrypt;
-import com.beehive.annotations.Injectable;
 import com.beehive.lib.service.Service;
 import com.beelog.user.entity.UserEntity;
-import com.beelog.user.service.UserNotFoundException;
+import com.beehive.annotations.Injectable;
 import com.beelog.user.service.UserService;
+import com.beelog.user.service.UserEmailAlreadyExistException;
+import com.beelog.user.service.UsernameAlreadyExistException;
 
 @Injectable
 public class AuthService extends Service {
 
   private final UserService userService = service(UserService.class);
 
-  public void register(String name, String username, String email, String password) {
-    userService.create(name, username, email, hashPassword(password));
+  private final PasswordService passwordService = service(PasswordService.class);
+
+  public UserEntity register(String name, String username, String email, String password)
+    throws UserEmailAlreadyExistException, UsernameAlreadyExistException {
+    String passwordHash = passwordService.hash(password);
+
+    return userService.create(name, username, email, passwordHash);
   }
 
-  public boolean login(String username, String password) {
-    try {
-      UserEntity user = userService.findByUsername(username);
-      return verifyPassword(password, user.getPasswordHash());
-    } catch (UserNotFoundException e) {
-      return false;
+  public boolean login(String username, String password) throws UserNotFoundException {
+    UserEntity user = userService.findByUsername(username);
+
+    if (user == null) {
+      throw new UserNotFoundException(username);
     }
+
+    return passwordService.validate(password, user.getPasswordHash());
   }
 
-  private String hashPassword(String password) {
-    return BCrypt.withDefaults().hashToString(12, password.toCharArray());
-  }
-
-  private boolean verifyPassword(String password, String hash) {
-    BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), hash);
-    return result.verified;
-  }
 }
