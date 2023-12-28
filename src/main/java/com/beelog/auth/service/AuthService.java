3ad2609911292caaf4d6b9bfe82087958793f68c
Halis Yücel <isaycel2001@gmail.com>
2023-12-28 12:53:02 +0300

    birtakım değişiklikler

--- src/main/java/com/beelog/auth/service/AuthService.java
+++ src/main/java/com/beelog/auth/service/AuthService.java
@@ -1,18 +1,22 @@
 package com.beelog.auth.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.beehive.annotations.Injectable;
 import com.beehive.lib.service.Service;
 import com.beelog.user.entity.UserEntity;
-import com.beehive.annotations.Injectable;
-import com.beelog.user.service.UserService;
 import com.beelog.user.service.UserEmailAlreadyExistException;
+import com.beelog.user.service.UserService;
 import com.beelog.user.service.UsernameAlreadyExistException;
+
+import java.util.UUID;
 
 @Injectable
 public class AuthService extends Service {
 
   private final UserService userService = service(UserService.class);
-
   private final PasswordService passwordService = service(PasswordService.class);
+  Algorithm algorithm = Algorithm.HMAC256("baeldung");
 
   public UserEntity register(String name, String username, String email, String password)
     throws UserEmailAlreadyExistException, UsernameAlreadyExistException {
@@ -21,14 +25,25 @@ public class AuthService extends Service {
     return userService.create(name, username, email, passwordHash);
   }
 
-  public boolean login(String username, String password) throws UserNotFoundException {
+  public String login(String username, String password) throws UserNotFoundException, InvalidCredentialsException {
     UserEntity user = userService.findByUsername(username);
 
     if (user == null) {
       throw new UserNotFoundException(username);
     }
 
-    return passwordService.validate(password, user.getPasswordHash());
+    boolean isValid = passwordService.validate(password, user.getPasswordHash());
+
+    if (!isValid) {
+      throw new InvalidCredentialsException();
+    }
+
+    return JWT.create()
+      .withClaim("username", username)
+      .withClaim("email", user.getEmail())
+      .withClaim("name", user.getName())
+      .withJWTId(UUID.randomUUID().toString())
+      .sign(algorithm);
   }
 
 }
